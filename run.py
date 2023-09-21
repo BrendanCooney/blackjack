a6bd9de88c5b287dd9807ad445c7c5c0af403b7b
BrendanCooney <advicast@gmail.com>
2023-09-21 21:49:57 +0000

    completed player turn function

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import random
 
-""""
+"""
 This function requests the players name before they play.
 The player name can be printed out after the game.
-""""
+"""
 
 def get_player_name():
   player_name = input("Enter your name:")
@@ -15,26 +15,27 @@ This is the Class called Card
 It has the attributes of suit (Clubs, Diamonds, Heards, Spades)
 It also has the attributes of rank (Ace,2..Jack,Queen,King)
 
-""""
+"""
 
 class Card:
 
-def __init__(self, suit, rank):
-    self.suit = suit
-    self.rank = rank
-    self.value = self.assign_value(rank)
+  def __init__(self, suit, rank):
+      self.suit = suit
+      self.rank = rank
+      self.value = self.assign_value(rank)
 
 """"
 This function returns the values of the rank and suit of the card.
 
-""""
+"""
+
 def __str__(self):
     return f"{self.rank} of {self.suit}"
 
 """"
 The assign_value  if - else function adds numerical values to the rank of the cards 
 
-""""
+"""
 
 def assign_value(self, rank):
     if rank in ["Jack", "Queen", "King"]:
@@ -55,12 +56,12 @@ The shuffle function shuffles cards using random.
 
 The deal function deals the cards taking the next card in the deck. 
 
-""""
+"""
 
-Class Deck: 
+class Deck:
 
-def__init__(self):
-self.cards = self.generate_deck()
+  def __init__(self):
+    self.cards = self.generate_deck()
 
 
   def generate_deck(self):
@@ -79,7 +80,7 @@ The Player Class is for the player in the card game.
 The attributes are the players name and the players hand. 
 The cards in the players hand must be printed to the console
 
-""""
+"""
 class Player:
 
   def __init__(self, name):
@@ -108,7 +109,8 @@ class Player:
 This is a class that represents the blackjack game.
 it involves the dealer dealing the player and dealers hands and then it checks for a win. 
 
-""""
+"""
+
 class Game:
 
   def __init__(self):
@@ -119,60 +121,49 @@ class Game:
 """"
 This function works on validating input 
 If the input is incorrect the function asks for "Hit" or "Stand"
-""""
-  def input_with_validation(self, prompt, valid_inputs):
-    while True:
-      user_input = input(prompt).lower().strip()
-      if user_input in valid_inputs:
-        return user_input
+"""
+
+def input_with_validation(self, prompt, valid_inputs):
+  while True:
+    user_input = input(prompt).lower().strip()
+    if user_input in valid_inputs:
+      return user_input
       print("Invalid choice. Please enter Hit or Stand.")
 
 """"
 This function starts the game play of blackjack.
 It lists other functions, shuffle, recieveing cards, players turn, dealers turn, evaluating values and 
 checking for the winner. 
-""""
-  def play(self):
-      self.deck.shuffle()
-      for _ in range(2):
-        self.player.receive_card(self.deck.deal())
-        self.dealer.receive_card(self.deck.deal())
-      self.player_turn()
-      if self.player.hand_value() <= 21:
-        self.dealer_turn()
-      self.check_winner()
-""""
-This function shows what happes when the player is dealt a card. 
-If the players initial hand is less than 21 it asks if the player wants to hit or stand. 
+"""
 
-""""
-
-  def player_turn(self):
-    while True:
-      print("Your hand:")
-      self.player.display_hand()
-      if self.player.hand_value() > 21:
-        print("Busted!")
-        return
-      action = input("Do you want to 'hit' or 'stand'? ").lower()
-      if action == 'hit':
-        self.player.receive_card(self.deck.deal())
-      elif action == 'stand':
-        return
-
-  """"
-  The dealer's turn function 
-
-  This function deals cards to the dealer until their hand value is at least 17.
-  """"     
-  def dealer_turn(self):
-
-    print("\nDealer's turn:")
-    while self.dealer.hand_value() < 17:
+def play(self):
+    self.deck.shuffle()
+    for _ in range(2):
+      self.player.receive_card(self.deck.deal())
       self.dealer.receive_card(self.deck.deal())
+    self.player_turn()
+    if self.player.hand_value() <= 21:
+      self.dealer_turn()
+    self.check_winner()
 
 """"
-The check_winner function checks who won the game.
+The players turn function 
+This function allows the player to hit or stand. If the player hits they are dealt a card if they stand it is the dealers turn. 
+"""
 
-The function compares the hand values of the player and the dealer and prints the result.
-""""
+def player_turn(self):
+  while True:
+    print("Your Hand:")
+    self.player.display_hand()
+
+    if self.player.hand_value() > 21:
+      print ("Busted!")
+      return
+
+    action = self.input_with_validation("Do you want to 'hit' or 'stand'?",
+                                            ['hit', 'stand'])
+
+    if action == 'hit':
+      self.player.receive_card(self.deck.deal())
+    elif action == 'stand':
+      return
